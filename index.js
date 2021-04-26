f888fc8b89ba9e1ab8da78f828067a067534fe8e
johnbryce <johnbryce@johnbryce.johmbryce>
2021-04-26 13:17:12 +0300

    260421_1317

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express')
+const fs = require('fs')
 // const { exec } = require("child_process");
 
 
 const app = express()
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} - ${req.url}, ${new Date().toISOString()}`)
-  next()
-}
-
 app.listen(2000, ()=>{
   console.log('server is running on port 2000')
-})
+  })
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`\n |${req.method} - ${req.url}, |${new Date().toISOString()}|`)
+    console.log(`req is ${req}`)
+    next()
+  })
+  
 const products = [
   {id: 1, name: 'blah 1', price:57.4},
   {id: 2, name: 'blah 2', price:95.4},
@@ -20,26 +24,44 @@ const products = [
   {id: 4, name: 'blah 4', price:15.4},
 ]
 
-app.get('/products/all',logger,(req,res)=>{
+let orders = [];
+
+
+app.get('/orders',(req,res)=>{
+  res.send(orders)
+})
+
+app.delete('/orders/:id', (req,res)=>{
+  orders = orders.filter(order=>order.id!==req.params.id);
+  res.send(orders);
+})
+
+app.post('/orders',(req,res)=>{
+  orders.push(req.body)
+  console.log(req.body)
+  res.send(orders)
+})
+
+app.get('/products/all',(req,res)=>{
   res.send(products)
 })
 
-app.get('/products/byid/:id',logger, (req,res)=>{
+app.get('/products/byid/:id', (req,res)=>{
   res.send(products.find(prod=>prod.id==req.params.id))
   
 })
 
 
-
 // exec("shutdown /r", (error, stdout, stderr) => {
-//   if (error) {
-//       console.log(`error: ${error.message}`);
-//       return;
-//   }
-//   if (stderr) {
-//       console.log(`stderr: ${stderr}`);
-//       return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-// });
-
+//     if (error) {
+//           console.log(`error: ${error.message}`);
+//           return;
+//       }
+//       if (stderr) {
+//             console.log(`stderr: ${stderr}`);
+//             return;
+//         }
+//         console.log(`stdout: ${stdout}`);
+//       });
+      
+      
