7b1cc9dd5c36e31684758c63612e7e31f1d56d30
Oren Sayag <orensayag500@gmail.com>
2021-05-24 16:31:20 +0300

    240521_1630

---
+++ server/DB/config.js
@@ -0,0 +1,33 @@
+// connect to db
+const mysql = require("mysql");
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "jb_34_bank",
+});
+
+con.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("Connected to mysql");
+});
+
+// wrapper
+const myQuery = (q) => {
+    return new Promise((resolve, reject)=>{
+        con.query(q,(err,results)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(results)
+            }
+        })
+    })
+}
+
+// EXPORT
+module.exports = { myQuery };
