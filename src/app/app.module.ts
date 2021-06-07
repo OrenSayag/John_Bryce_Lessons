21debf2c0b2732a504b561c3274b266260869d86
johnbryce <johnbryce@johnbryce.johmbryce>
2021-06-07 13:38:10 +0300

    070621_1338

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './components/top/top.component';
+import { LeftComponent } from './components/left/left.component';
+import { RightComponent } from './components/right/right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule,
