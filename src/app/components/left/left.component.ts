1dd7a2484a4bacad3859c49c207a43d266819981
johnbryce <johnbryce@johnbryce.johmbryce>
2021-06-09 09:19:59 +0300

    090621_0919

--- src/app/components/left/left.component.ts
+++ src/app/components/left/left.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftComponent implements OnInit {
 
+  public links:string[] = ['Home', 'Shop', 'About', 'Contact Us']
+
   constructor() { }
 
 
--- src/app/components/right/right.component.ts
+++ src/app/components/right/right.component.ts
@@ -12,6 +12,7 @@ export class RightComponent implements OnInit {
   public title:string = "Shopping list"
   public imgSrc:string = "https://media.wired.com/photos/5c9040ee4950d24718d6da99/master/w_2560%2Cc_limit/shoppingcart-1066110386.jpg"
   public imgWidth:number = 300
+  public userType:string = 'admin'
 
   public dec():void{
     this.imgWidth -= 10
